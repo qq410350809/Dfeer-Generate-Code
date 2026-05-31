68756945a3edebb98b4deb7c171aad490ccc2f8e
wudixm <410350809@qq.com>
2026-05-31 20:50:28 +0800

    fix: 调整 since-build 为 231 (2023.1) 以兼容 addBrowseFolderListener API

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
 intellijPlatform {
     pluginConfiguration {
         ideaVersion {
-            sinceBuild = "221"
+            sinceBuild = "231"
             untilBuild = "263.*"
         }
     }
